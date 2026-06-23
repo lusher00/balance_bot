64809f8155d21d59c40cf5132ff8577699155a8c
Ryan Lush <ryan.lush@gmail.com>
2026-06-22 21:18:33 -0400

    estop io names updated for new kernel

--- estop_clear.sh
+++ estop_clear.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-GPIO=/sys/class/gpio/gpio57
+GPIO=/sys/class/gpio/gpio569
 if [ ! -d "$GPIO" ]; then
-    echo 57 > /sys/class/gpio/export
+    echo 569 > /sys/class/gpio/export
     sleep 0.15
 fi
 echo out > ${GPIO}/direction
 echo 1   > ${GPIO}/value
-echo "E-stop cleared, GPIO57=1"
+echo "E-stop cleared, GPIO569=1"
